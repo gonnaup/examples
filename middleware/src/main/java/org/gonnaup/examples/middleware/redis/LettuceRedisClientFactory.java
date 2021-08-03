e27bc9f5ca3aa0582bdcb28d889a77fb76278205
gonnaup <gonnaup@163.com>
2021-08-03 13:24:17 +0800

    redis链接参数改为properties文件读取

--- middleware/src/main/java/org/gonnaup/examples/middleware/redis/LettuceRedisClientFactory.java
+++ middleware/src/main/java/org/gonnaup/examples/middleware/redis/LettuceRedisClientFactory.java
@@ -1,7 +1,12 @@
 package org.gonnaup.examples.middleware.redis;
 
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * 客户端工厂类
@@ -14,10 +19,19 @@ public class LettuceRedisClientFactory {
 
     /**
      * 连接独立的redis服务端
+     *
      * @return
      */
     public static RedisClient standaloneClient() {
-        return RedisClient.create("redis://192.168.0.8/0?clientName=gonnaup");
+        Properties prop = new Properties();
+        try (final InputStream resource = ClassLoader.getSystemResourceAsStream("redis.properties")) {
+            prop.load(resource);
+            final RedisURI redisURI = RedisURI.builder().withHost(prop.getProperty("host")).withPort(Integer.parseInt(prop.getProperty("port"))).withClientName("gonnaup").build();
+            return RedisClient.create(redisURI);
+        } catch (IOException e) {
+            log.error("load redis.properties failed => {}", e.getMessage());
+        }
+        throw new RuntimeException("connect redis failed! properties => " + prop);
     }
 
 }
