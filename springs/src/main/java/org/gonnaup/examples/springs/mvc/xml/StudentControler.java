b9e141f90d6a9f7d2a85502d31a2cc68399bb6c5
gonnaup <gonnaup@163.com>
2021-08-23 16:21:25 +0800

    spring mvc 配置utf-8转换器

--- springs/src/main/java/org/gonnaup/examples/springs/mvc/xml/StudentControler.java
+++ springs/src/main/java/org/gonnaup/examples/springs/mvc/xml/StudentControler.java
@@ -1,26 +1,36 @@
 package org.gonnaup.examples.springs.mvc.xml;
 
 import org.gonnaup.examples.springs.db.hibernate.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author gonnaup
  * @version created at 2021/8/20 21:19
  */
-@RestController
+@Controller
 public class StudentControler {
 
 
     @GetMapping("/student/list")
-    public Student listOne() {
+    public String listOne(Model model) {
         Student student = new Student();
         student.setId(10000);
         student.setName("张三");
         student.setAge(25);
         student.setCourse("计算机");
         student.setScore(99);
-        return student;
+        model.addAttribute("student", student);
+        return "student";
+    }
+
+    @GetMapping("/printname")
+    @ResponseBody
+    public String printname(@RequestParam("name") String name) {
+        return name;
     }
 
 }
---
+++ springs/src/main/webapp/WEB-INF/views/student.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gonnaup
+  Date: 2021/8/22
+  Time: 10:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>student</title>
+</head>
+<body style="background: aquamarine">
+
+用户信息${student}
+
+</body>
+</html>
